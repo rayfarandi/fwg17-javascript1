38929fde14beb8c16c177942e7bb3868533a15c9
Ran Ran <43269555+rayfarandi@users.noreply.github.com>
2023-10-24 16:36:42 +0700

    tugas 1

---
+++ tugas1.js
@@ -0,0 +1,45 @@
+const biodata = {
+    name: "Ray Frandi",
+    age: 29,
+    hobbies: ["Video Game", "Otomotif", "Olahraga"],
+    isMarried: true,
+    schoolList: [
+      {
+        name: "SMA Setia Negara",
+        yearIn: 2009,
+        yearOut: 2012,
+        major: null
+      },
+      {
+        name: "UNIVERSITAS GUNADARMA",
+        yearIn: 2012,
+        yearOut: 2017,
+        major: "SISTEM INFORMASI"
+      }
+    ],
+    skills: [
+      {
+        skillName: "JavaScript",
+        level: "Biginer"
+      },
+      {
+        skillName: "HTML",
+        level: "beginner"
+      },
+      {
+        skillName: "CSS",
+        level: "beginner"
+      }
+    ],
+    interestInCoding: true
+  };
+  
+  // Contoh penggunaan
+  console.log(biodata.name);
+  console.log(biodata.age);
+  console.log(biodata.hobbies);
+  console.log(biodata.isMarried);
+  console.log(biodata.schoolList);
+  console.log(biodata.skills);
+  console.log(biodata.interestInCoding);
+  
